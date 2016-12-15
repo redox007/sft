50b62145821fef6b4294aebcf2fd66df247c86ac
Samik Saha <visitsameek@gmail.com>
2016-12-15 21:20:45 +0530

    img size
    14-12-16

--- application/config/media.php
+++ application/config/media.php
@@ -10,4 +10,12 @@ $config['media_sizes'] = array(
     'small' => [250, 100],
     'large' => [800, 600],
     'media_thumb' => [350, 220],
+    'thumb' => [100, 80],
+	'type' => [380, 700],
+	'square' => [360, 320],
+	'banner' => [1920, 950],
+	'type' => [180, 152],
+	'library' => [451, 600],
+	'best_offer' => [480, 400],
+	'ajmj' => [961, 541]
 );
